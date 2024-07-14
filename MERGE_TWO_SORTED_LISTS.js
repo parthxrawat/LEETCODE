8855f4dc9f5e5d837848dd79af39ee59840fae2c
Parth Rawat <parthrawat2001@gmail.com>
2024-07-14 14:56:08 +0530

    Create MERGE_TWO_SORTED_LISTS.js

---
+++ MERGE_TWO_SORTED_LISTS.js
@@ -0,0 +1,29 @@
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    let dummy = new ListNode();
+    let current = dummy;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    current.next = l1 || l2;
+
+    return dummy.next;
+}
